39e7661636ba6eb2a17648ab03ae731b02cdde3b
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2020-07-08 01:14:15 -0500

    adding nprogress

--- helpers/slug.js
+++ helpers/slug.js
@@ -2,6 +2,9 @@ const slug = (name = '') =>
   name
     .toLocaleLowerCase()
     .replace(/\s/g, '-')
+    .replace(/[^a-z0-9 -]/g, '')
+    .replace(/\s+/g, '-')
+    .replace(/-+/g, '-')
     .normalize('NFD')
     .replace(/[\u0300-\u036f]/g, '');
 
---
+++ hooks/useLoading.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { useRouter } from 'next/router';
+import NProgress from 'nprogress';
+
+export const useLoading = () => {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const handleStart = () => NProgress.start();
+    const handleComplete = () => NProgress.done();
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+  });
+};
